011f6080b4ec4cf84a526e81d0b45564ff89d406
mizzie221 <mi852768@ucf.edu>
2024-08-18 00:59:38 -0400

    new

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
+import Header from './components/Header';
 
 function App() {
   return (
     <div>
-      <h1>Hello, World!</h1>
-      <p>If you see this, React is working!</p>
+      <Header />
     </div>
   );
 }
