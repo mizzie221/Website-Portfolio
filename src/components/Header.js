7fe671fefbe961526f9eca2bf8e385057e841a1b
mizzie221 <mi852768@ucf.edu>
2024-08-22 20:36:19 -0400

    Fixed Code

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navigation from './Navigation';  // Ensure this is correct
+
+const Header = () => {
+  return (
+    <header>
+      <h1>This is the Header</h1>
+      <Navigation />
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Home.scss';
+
+const Home = () => {
+  return (
+    <section id="home">
+      <div>
+        <h2>Welcome to My Portfolio</h2>
+      </div>
+    </section>
+  );
+};
+
+export default Home;
