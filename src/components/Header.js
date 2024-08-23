69a1a8b01a98189bdc6b88ead0c9b9e3c156719d
mizzie221 <mi852768@ucf.edu>
2024-08-22 20:37:00 -0400

    New

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import Navigation from './Navigation';  // Ensure this is correct
+// import './Header.scss';
 
 const Header = () => {
   return (
-    <header>
-      <h1>This is the Header</h1>
-      <Navigation />
+    <header className="header">
+      <div className="container">
+        <h1>Hello, I'm <span className="name">Minh Pham</span></h1>
+        <p className="tagline">I love Coding!<span id="dynamic-text"></span></p>
+        <div className="buttons">
+          <a href="#work" className="btn">View my work</a>
+          <a href="#resume" className="btn">View Resume</a>
+        </div>
+      </div>
     </header>
   );
 };
