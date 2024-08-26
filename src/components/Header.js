3b668756a061e7e564125f9af7f8c08d8fe8d65f
mizzie221 <mi852768@ucf.edu>
2024-08-25 23:41:10 -0400

    Added TV

--- src/components/Header.js
+++ src/components/Header.js
@@ -75,6 +75,7 @@ const Header = () => {
       <div className="dvd-logo-container">
         <img ref={logoRef} src={logos[currentLogoIndex]} alt="Logo" className="dvd-logo" />
       </div>
+
       <div className="centered-content">
         <h1>Hi, I'm Minh</h1>
         <div className="typing-container">
@@ -92,80 +93,88 @@ const Header = () => {
             onStringTyped={(index, self) => handleTyping(self.strings[index])}
           />
         </div>
-        <div className="keyboard-container">
-          <div className="row">
-            <div className="key esc">ESC</div>
-            <div className={`key ${pressedKey === '1' ? 'pressed' : ''}`}>1</div>
-            <div className={`key ${pressedKey === '2' ? 'pressed' : ''}`}>2</div>
-            <div className={`key ${pressedKey === '3' ? 'pressed' : ''}`}>3</div>
-            <div className={`key ${pressedKey === '4' ? 'pressed' : ''}`}>4</div>
-            <div className={`key ${pressedKey === '5' ? 'pressed' : ''}`}>5</div>
-            <div className={`key ${pressedKey === '6' ? 'pressed' : ''}`}>6</div>
-            <div className={`key ${pressedKey === '7' ? 'pressed' : ''}`}>7</div>
-            <div className={`key ${pressedKey === '8' ? 'pressed' : ''}`}>8</div>
-            <div className={`key ${pressedKey === '9' ? 'pressed' : ''}`}>9</div>
-            <div className={`key ${pressedKey === '0' ? 'pressed' : ''}`}>0</div>
-            <div className={`key ${pressedKey === '-' ? 'pressed' : ''}`}>-</div>
-            <div className={`key ${pressedKey === '=' ? 'pressed' : ''}`}>=</div>
-            <div className="key backspace">Backspace</div>
-          </div>
-          <div className="row">
-            <div className="key tab">Tab</div>
-            <div className={`key ${pressedKey === 'Q' ? 'pressed' : ''}`}>Q</div>
-            <div className={`key ${pressedKey === 'W' ? 'pressed' : ''}`}>W</div>
-            <div className={`key ${pressedKey === 'E' ? 'pressed' : ''}`}>E</div>
-            <div className={`key ${pressedKey === 'R' ? 'pressed' : ''}`}>R</div>
-            <div className={`key ${pressedKey === 'T' ? 'pressed' : ''}`}>T</div>
-            <div className={`key ${pressedKey === 'Y' ? 'pressed' : ''}`}>Y</div>
-            <div className={`key ${pressedKey === 'U' ? 'pressed' : ''}`}>U</div>
-            <div className={`key ${pressedKey === 'I' ? 'pressed' : ''}`}>I</div>
-            <div className={`key ${pressedKey === 'O' ? 'pressed' : ''}`}>O</div>
-            <div className={`key ${pressedKey === 'P' ? 'pressed' : ''}`}>P</div>
-            <div className={`key ${pressedKey === '[' ? 'pressed' : ''}`}>[</div>
-            <div className={`key ${pressedKey === ']' ? 'pressed' : ''}`}>]</div>
-            <div className={`key ${pressedKey === '\\' ? 'pressed' : ''}`}>\\</div>
-          </div>
-          <div className="row">
-            <div className="key capslock">Caps</div>
-            <div className={`key ${pressedKey === 'A' ? 'pressed' : ''}`}>A</div>
-            <div className={`key ${pressedKey === 'S' ? 'pressed' : ''}`}>S</div>
-            <div className={`key ${pressedKey === 'D' ? 'pressed' : ''}`}>D</div>
-            <div className={`key ${pressedKey === 'F' ? 'pressed' : ''}`}>F</div>
-            <div className={`key ${pressedKey === 'G' ? 'pressed' : ''}`}>G</div>
-            <div className={`key ${pressedKey === 'H' ? 'pressed' : ''}`}>H</div>
-            <div className={`key ${pressedKey === 'J' ? 'pressed' : ''}`}>J</div>
-            <div className={`key ${pressedKey === 'K' ? 'pressed' : ''}`}>K</div>
-            <div className={`key ${pressedKey === 'L' ? 'pressed' : ''}`}>L</div>
-            <div className={`key ${pressedKey === ';' ? 'pressed' : ''}`}>;</div>
-            <div className={`key ${pressedKey === '\'' ? 'pressed' : ''}`}>'</div>
-            <div className="key enter">Enter</div>
-          </div>
-          <div className="row">
-            <div className="key shift">Shift</div>
-            <div className={`key ${pressedKey === 'Z' ? 'pressed' : ''}`}>Z</div>
-            <div className={`key ${pressedKey === 'X' ? 'pressed' : ''}`}>X</div>
-            <div className={`key ${pressedKey === 'C' ? 'pressed' : ''}`}>C</div>
-            <div className={`key ${pressedKey === 'V' ? 'pressed' : ''}`}>V</div>
-            <div className={`key ${pressedKey === 'B' ? 'pressed' : ''}`}>B</div>
-            <div className={`key ${pressedKey === 'N' ? 'pressed' : ''}`}>N</div>
-            <div className={`key ${pressedKey === 'M' ? 'pressed' : ''}`}>M</div>
-            <div className={`key ${pressedKey === ',' ? 'pressed' : ''}`}>,</div>
-            <div className={`key ${pressedKey === '.' ? 'pressed' : ''}`}>.</div>
-            <div className={`key ${pressedKey === '/' ? 'pressed' : ''}`}>/</div>
-            <div className="key shift">Shift</div>
-          </div>
-          <div className="row">
-            <div className="key ctrl">Ctrl</div>
-            <div className="key win">Win</div>
-            <div className="key alt">Alt</div>
-            <div className="key space ${pressedKey === ' ' ? 'pressed' : ''}"></div>
-            <div className="key alt">Alt</div>
-            <div className="key win">Win</div>
-            <div className="key menu">Menu</div>
-            <div className="key ctrl">Ctrl</div>
+
+        <div className="tv-screen">
+          <div className="screen-content">
+            
           </div>
         </div>
 
+        <div className="keyboard-wrapper">
+          <div className="keyboard-container">
+            <div className="row">
+              <div className="key esc" data-key="ESC">ESC</div>
+              <div className={`key ${pressedKey === '1' ? 'pressed' : ''}`} data-key="1">1</div>
+              <div className={`key ${pressedKey === '2' ? 'pressed' : ''}`} data-key="2">2</div>
+              <div className={`key ${pressedKey === '3' ? 'pressed' : ''}`} data-key="3">3</div>
+              <div className={`key ${pressedKey === '4' ? 'pressed' : ''}`} data-key="4">4</div>
+              <div className={`key ${pressedKey === '5' ? 'pressed' : ''}`} data-key="5">5</div>
+              <div className={`key ${pressedKey === '6' ? 'pressed' : ''}`} data-key="6">6</div>
+              <div className={`key ${pressedKey === '7' ? 'pressed' : ''}`} data-key="7">7</div>
+              <div className={`key ${pressedKey === '8' ? 'pressed' : ''}`} data-key="8">8</div>
+              <div className={`key ${pressedKey === '9' ? 'pressed' : ''}`} data-key="9">9</div>
+              <div className={`key ${pressedKey === '0' ? 'pressed' : ''}`} data-key="0">0</div>
+              <div className={`key ${pressedKey === '-' ? 'pressed' : ''}`} data-key="-">-</div>
+              <div className={`key ${pressedKey === '=' ? 'pressed' : ''}`} data-key="=">=</div>
+              <div className="key backspace" data-key="Back">Backspace</div>
+            </div>
+            <div className="row">
+              <div className="key tab" data-key="Tab">Tab</div>
+              <div className={`key ${pressedKey === 'Q' ? 'pressed' : ''}`} data-key="Q">Q</div>
+              <div className={`key ${pressedKey === 'W' ? 'pressed' : ''}`} data-key="W">W</div>
+              <div className={`key ${pressedKey === 'E' ? 'pressed' : ''}`} data-key="E">E</div>
+              <div className={`key ${pressedKey === 'R' ? 'pressed' : ''}`} data-key="R">R</div>
+              <div className={`key ${pressedKey === 'T' ? 'pressed' : ''}`} data-key="T">T</div>
+              <div className={`key ${pressedKey === 'Y' ? 'pressed' : ''}`} data-key="Y">Y</div>
+              <div className={`key ${pressedKey === 'U' ? 'pressed' : ''}`} data-key="U">U</div>
+              <div className={`key ${pressedKey === 'I' ? 'pressed' : ''}`} data-key="I">I</div>
+              <div className={`key ${pressedKey === 'O' ? 'pressed' : ''}`} data-key="O">O</div>
+              <div className={`key ${pressedKey === 'P' ? 'pressed' : ''}`} data-key="P">P</div>
+              <div className={`key ${pressedKey === '[' ? 'pressed' : ''}`} data-key="[">[</div>
+              <div className={`key ${pressedKey === ']' ? 'pressed' : ''}`} data-key="]">]</div>
+              <div className={`key ${pressedKey === '\\' ? 'pressed' : ''}`} data-key="\\">\\</div>
+            </div>
+            <div className="row">
+              <div className="key capslock" data-key="Caps">Caps</div>
+              <div className={`key ${pressedKey === 'A' ? 'pressed' : ''}`} data-key="A">A</div>
+              <div className={`key ${pressedKey === 'S' ? 'pressed' : ''}`} data-key="S">S</div>
+              <div className={`key ${pressedKey === 'D' ? 'pressed' : ''}`} data-key="D">D</div>
+              <div className={`key ${pressedKey === 'F' ? 'pressed' : ''}`} data-key="F">F</div>
+              <div className={`key ${pressedKey === 'G' ? 'pressed' : ''}`} data-key="G">G</div>
+              <div className={`key ${pressedKey === 'H' ? 'pressed' : ''}`} data-key="H">H</div>
+              <div className={`key ${pressedKey === 'J' ? 'pressed' : ''}`} data-key="J">J</div>
+              <div className={`key ${pressedKey === 'K' ? 'pressed' : ''}`} data-key="K">K</div>
+              <div className={`key ${pressedKey === 'L' ? 'pressed' : ''}`} data-key="L">L</div>
+              <div className={`key ${pressedKey === ';' ? 'pressed' : ''}`} data-key=";">;</div>
+              <div className={`key ${pressedKey === '\'' ? 'pressed' : ''}`} data-key="'">'</div>
+              <div className="key enter" data-key="Enter">Enter</div>
+            </div>
+            <div className="row">
+              <div className="key shift" data-key="Shift">Shift</div>
+              <div className={`key ${pressedKey === 'Z' ? 'pressed' : ''}`} data-key="Z">Z</div>
+              <div className={`key ${pressedKey === 'X' ? 'pressed' : ''}`} data-key="X">X</div>
+              <div className={`key ${pressedKey === 'C' ? 'pressed' : ''}`} data-key="C">C</div>
+              <div className={`key ${pressedKey === 'V' ? 'pressed' : ''}`} data-key="V">V</div>
+              <div className={`key ${pressedKey === 'B' ? 'pressed' : ''}`} data-key="B">B</div>
+              <div className={`key ${pressedKey === 'N' ? 'pressed' : ''}`} data-key="N">N</div>
+              <div className={`key ${pressedKey === 'M' ? 'pressed' : ''}`} data-key="M">M</div>
+              <div className={`key ${pressedKey === ',' ? 'pressed' : ''}`} data-key=",">,</div>
+              <div className={`key ${pressedKey === '.' ? 'pressed' : ''}`} data-key=".">.</div>
+              <div className={`key ${pressedKey === '/' ? 'pressed' : ''}`} data-key="/">/</div>
+              <div className="key shift" data-key="Shift">Shift</div>
+            </div>
+            <div className="row">
+              <div className="key ctrl" data-key="Ctrl">Ctrl</div>
+              <div className="key win" data-key="Win">Win</div>
+              <div className="key alt" data-key="Alt">Alt</div>
+              <div className="key space" data-key=" "></div>
+              <div className="key alt" data-key="Alt">Alt</div>
+              <div className="key win" data-key="Win">Win</div>
+              <div className="key menu" data-key="Menu">Menu</div>
+              <div className="key ctrl" data-key="Ctrl">Ctrl</div>
+            </div>
+          </div>
+        </div>
       </div>
     </header>
   );
